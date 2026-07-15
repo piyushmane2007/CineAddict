5ec53ea9ef41ae7b980c50c40f9eed08280e0d90
Piyush Mane <piyushmanekarad123@gmail.com>
2026-07-15 12:07:13 +0530

    Complete movie module with TMDb integration

--- backend/routes/movies.py
+++ backend/routes/movies.py
@@ -1,5 +1,5 @@
 from flask import Blueprint,jsonify,request
-from services.tmbd import get_trending_movies,search_movies,get_movie_details,get_movie_trailer,get_movie_credits
+from services.tmbd import get_trending_movies,search_movies,get_movie_details,get_movie_trailer,get_movie_credits,get_movie_recommendations,get_popular_movies,get_top_rated_movies,get_upcoming_movies,get_now_playing_movies,get_similar_movies,get_movie_reviews,get_discover_movies,get_movie_genres
 
 
 movies_bp = Blueprint("movies",__name__)
@@ -35,3 +35,53 @@ def movie_credits(movie_id):
     credits =  get_movie_credits(movie_id) 
     return jsonify(credits) 
 
+@movies_bp.route("/<int:movie_id>/recommendations")
+def movie_recommendations(movie_id):
+    recommendations =  get_movie_recommendations(movie_id) 
+    return jsonify(recommendations) 
+
+@movies_bp.route("/popular",methods=["GET"])
+def popular_movies():
+    popular = get_popular_movies()
+    return jsonify(popular) 
+
+@movies_bp.route("/top_rated",methods=["GET"])
+def top_rated_movies():
+    top_rated = get_top_rated_movies()
+    return jsonify(top_rated)
+
+@movies_bp.route("/upcoming",methods=["GET"])
+def upcoming_movies():
+    upcoming = get_upcoming_movies()
+    return jsonify(upcoming) 
+
+@movies_bp.route("/now_playing",methods=["GET"])
+def now_playing_movies():
+    now_playing = get_now_playing_movies()
+    return jsonify(now_playing) 
+
+@movies_bp.route("/<int:movie_id>/similar")
+def similar_movies(movie_id):
+    similar =  get_similar_movies(movie_id) 
+    return jsonify(similar) 
+
+
+@movies_bp.route("/<int:movie_id>/reviews")
+def movie_review(movie_id):
+    review =  get_movie_reviews(movie_id) 
+    return jsonify(review)
+
+@movies_bp.route("/genres",methods=["GET"])
+def movie_genres():
+    genres =  get_movie_genres()
+    return jsonify(genres) 
+
+@movies_bp.route("/discover", methods=["GET"])
+def discover_movies():
+    genre = request.args.get("genre")
+
+    if not genre:
+        return jsonify({"error": "Genre parameter is required"}), 400
+
+    results = get_discover_movies(genre)
+    return jsonify(results)
--- backend/services/tmbd.py
+++ backend/services/tmbd.py
@@ -93,4 +93,167 @@ def get_movie_credits(movie_id):
     return {
         "director":director,
         "cast":cast
-    }
+    } 
+
+def get_movie_recommendations(movie_id):
+    api_key =  os.getenv("TMDB_API_KEY") 
+    params = {
+        "api_key" : api_key , 
+        
+    }  
+    url = f"{BASE_URL}/movie/{movie_id}/recommendations"
+    response = requests.get(url,params=params) 
+    response.raise_for_status()
+    data = response.json() 
+    movies = []
+
+    for recommended_movie in data["results"][:5]:
+        movies.append({
+        "id": recommended_movie["id"],
+        "title": recommended_movie["title"],
+        "poster_path": recommended_movie["poster_path"],
+        "vote_average": recommended_movie["vote_average"]
+    })
+
+    return movies 
+
+def get_popular_movies():
+    api_key =  os.getenv("TMDB_API_KEY") 
+    params = {
+        "api_key" : api_key , 
+        
+    } 
+    url = f"{BASE_URL}/movie/popular" 
+    response = requests.get(url,params=params) 
+    response.raise_for_status()
+    data = response.json() 
+    return data
+    
+
+def get_top_rated_movies():
+    api_key =  os.getenv("TMDB_API_KEY")
+    params = {
+        "api_key" : api_key , 
+        
+    } 
+    url = f"{BASE_URL}/movie/top_rated"
+    response = requests.get(url,params=params) 
+    response.raise_for_status()
+    data = response.json() 
+    return data
+
+def get_upcoming_movies():
+    api_key =  os.getenv("TMDB_API_KEY")
+    params = {
+        "api_key" : api_key , 
+        
+    } 
+    url = f"{BASE_URL}/movie/upcoming"
+    response = requests.get(url,params=params) 
+    response.raise_for_status()
+    data = response.json() 
+    return data
+
+def get_now_playing_movies():
+
+    api_key =  os.getenv("TMDB_API_KEY")
+    params = {
+        "api_key" : api_key , 
+        
+    } 
+    url = f"{BASE_URL}/movie/now_playing"   
+    response = requests.get(url,params=params) 
+    response.raise_for_status()
+    data = response.json() 
+    return data
+
+
+def get_similar_movies(movie_id):
+    api_key =  os.getenv("TMDB_API_KEY") 
+    params = {
+        "api_key" : api_key , 
+        
+    }  
+    url = f"{BASE_URL}/movie/{movie_id}/similar"
+    response = requests.get(url,params=params) 
+    response.raise_for_status()
+    data = response.json() 
+    movies = []
+
+    for similar_movie in data["results"][:5]:
+        movies.append({
+        "id":similar_movie["id"],
+        "title":similar_movie["title"],
+        "poster_path":similar_movie["poster_path"],
+        "vote_average":similar_movie["vote_average"]
+    })
+
+    return movies   
+
+
+def get_movie_reviews(movie_id):
+    api_key =  os.getenv("TMDB_API_KEY") 
+    params = {
+        "api_key" : api_key , 
+        
+    }  
+    url = f"{BASE_URL}/movie/{movie_id}/reviews"
+    response = requests.get(url,params=params) 
+    response.raise_for_status()
+    data = response.json() 
+    reviews = []
+    for review_content in data["results"][:5]:
+        reviews.append({
+            "author":review_content["author"],
+            "rating": review_content["author_details"].get("rating"),
+            "content":review_content["content"]
+
+        })
+    return reviews   
+
+def get_movie_genres():
+
+    api_key =  os.getenv("TMDB_API_KEY")
+    params = {
+        "api_key" : api_key , 
+        
+    } 
+    url = f"{BASE_URL}/genre/movie/list"
+    response = requests.get(url,params=params) 
+    response.raise_for_status()
+    data = response.json() 
+    return data["genres"] 
+
+
+def get_discover_movies(genre):
+
+    api_key =  os.getenv("TMDB_API_KEY")
+    params = {
+        "api_key" : api_key , 
+        "with_genres": genre
+        
+    } 
+    url = f"{BASE_URL}/discover/movie"
+    response = requests.get(url,params=params) 
+    response.raise_for_status()
+    data = response.json() 
+    discover = []
+    for discover_content in data["results"][:5]:
+        discover.append({
+            "id":discover_content["id"],
+        "title":discover_content["title"],
+        "poster_path":discover_content["poster_path"],
+        "vote_average":discover_content["vote_average"]
+
+        })
+    return discover 
+
+
+
+
+
+
+
+
+
+    
