d9ca3206cd2162e503f539654402ffb00c3a7f1b
Piyush Mane <piyushmanekarad123@gmail.com>
2026-07-17 18:29:50 +0530

    Implement complete user CRUD with SQLAlchemy and PostgreSQL

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,26 @@
-from flask import Flask
+from flask import Flask 
+from config import Config 
+from models.db import db
+from models.user import User
+from routes.user import user_bp
 from routes.movies import movies_bp
 
 app = Flask(__name__)
+
+app.config.from_object(Config)
+
+db.init_app(app)
+
 app.register_blueprint(movies_bp, url_prefix="/api/movies")
+
+app.register_blueprint(user_bp, url_prefix="/api/users")
 
 @app.route("/")
 def home():
     return "CineAddict Backend is Running!"
 
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
